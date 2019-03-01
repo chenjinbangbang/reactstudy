0953b3e36f9b5bda9154e70e6976818cb865fb3d
陈进帮 <ex-chenjinbang@dashuf.com>
2019-03-01 20:57:53 +0800

    学习antd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 // import logo from './assets/images/logo.svg';
 import "./assets/css/App.css";
 
+import { Button } from 'antd';
+
 // 引入组件
 import Home from "./components/Home";
 import ReactForm from "./components/ReactForm";
@@ -44,8 +46,9 @@ class App extends Component {
       //   {/* 父-子组件传值，方法和整个组件 */}
       //   {/* <Header title="父组件的值" run={this.run} news={this} /> */}
       // </div>
-      // <Router>
+      <Router>
         <div>
+          <Button type="primary">primary</Button>
           <Link to="/">Home</Link> 
           <Link to={`/reactform?title=${this.state.title}`}>ReactForm</Link> 
           <Link to={`/todolist/${this.state.title}`}>Todolist</Link> 
@@ -54,7 +57,7 @@ class App extends Component {
           <Route exact path="/reactform" component={ReactForm}></Route>
           <Route exact path="/todolist/:id" component={Todolist}></Route>
         </div>
-      // </Router>
+      </Router>
     );
   }
 }
